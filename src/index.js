debb82400bc726c05d64d30ac75c69ebc9367673
Berucha (C284934) <142602387+berucha-lilly@users.noreply.github.com>
2024-09-11 16:32:26 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.css'; 
-import App from './App';
+import './css/index.css';  // This imports global styles
+import App from './App';   // This is the main component of our application
 
+// Creating a root for our React application
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render our App component
+// React.StrictMode is a tool for highlighting potential problems in an application
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
