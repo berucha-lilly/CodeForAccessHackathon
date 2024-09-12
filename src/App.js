168bf49eea68963b16688488912e76af482ec3b0
Berucha (C284934) <142602387+berucha-lilly@users.noreply.github.com>
2024-09-11 21:40:25 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   useEffect(() => {
     // To change the active component, modify the string below to one of:
     // 'ContrastToggle', 'FormValidation', or 'KeyboardNav'
-    const selectedComponent = 'ContrastToggle';
+    const selectedComponent = 'FormValidation';
     setActiveComponent(selectedComponent);
   }, []);
 
